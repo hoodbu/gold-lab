44d0b286bccc9740390de6adccbd8ddbee8ab766
Umair Hoodbhoy <hoodbu@gmail.com>
2020-11-20 21:55:40 -0500

    added 'depends on' for AWS-GCP peering

--- mcna/peering.tf
+++ mcna/peering.tf
@@ -9,4 +9,5 @@ resource "aviatrix_transit_gateway_peering" "aws_azure" {
 resource "aviatrix_transit_gateway_peering" "aws_gcp" {
   transit_gateway_name1 = var.aws_transit_gateway.name
   transit_gateway_name2 = var.gcp_transit_gateway.name
+  depends_on            = [aviatrix_transit_gateway.aws_transit_gw, aviatrix_transit_gateway.gcp_transit_gw]
 }
